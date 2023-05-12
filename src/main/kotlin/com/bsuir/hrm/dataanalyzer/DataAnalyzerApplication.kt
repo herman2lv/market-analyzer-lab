5f36779cda09c6d66a0e6d883a11d8750961fa84
herman2lv <herman2lv@gmail.com>
2023-05-12 23:12:28 +0300

    Add RestTemplate bean

--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/DataAnalyzerApplication.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/DataAnalyzerApplication.kt
@@ -2,9 +2,19 @@ package com.bsuir.hrm.dataanalyzer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class DataAnalyzerApplication
+class DataAnalyzerApplication {
+
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder.build()
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<DataAnalyzerApplication>(*args)
