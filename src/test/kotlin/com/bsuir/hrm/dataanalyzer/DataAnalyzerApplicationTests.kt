6c6c40a222e961a5cb5b730e35ae19d49d6a3f75
herman2lv <herman2lv@gmail.com>
2023-05-12 20:38:12 +0300

    Initial commit

---
+++ src/test/kotlin/com/bsuir/hrm/dataanalyzer/DataAnalyzerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bsuir.hrm.dataanalyzer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataAnalyzerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
