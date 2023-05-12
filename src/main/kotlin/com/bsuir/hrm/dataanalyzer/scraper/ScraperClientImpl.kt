9afebcea90a3c06577d23579214d34d1d66e7638
herman2lv <herman2lv@gmail.com>
2023-05-13 00:17:50 +0300

    ScraperClientImpl: minor refactoring

--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/scraper/ScraperClientImpl.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/scraper/ScraperClientImpl.kt
@@ -10,45 +10,67 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoField
 
+private const val FIRST_DAY_OF_MONTH = 1
+
 @Component
 class ScraperClientImpl(
     val restTemplate: RestTemplate,
     val jsonMapper: ObjectMapper,
 ) : ScraperClient {
-    val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
+    val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")//FIXME externalize props
 
     override fun getProducts(category: String, page: Int): ArrayList<Product> {
-        val url = "https://catalog.onliner.by/sdapi/catalog.api/search/${category}?page=${page}&order=price:asc"
-        val httpEntity: HttpEntity<String> = restTemplate.getForEntity(url, String::class.java)
-        val json: JsonNode = jsonMapper.readTree(httpEntity.body)
-        val products = arrayListOf<Product>()
-        json["products"].onEach {
-            val id = it["id"].asLong()
-            val key = it["key"].asText()
-            val name = it["full_name"].asText()
-            val jsonPrice = it["prices"]["price_min"]
-            val price = Money(toDecimal(jsonPrice["amount"]), Currency.valueOf(jsonPrice["currency"].asText()))
-            products.add(Product(id, key, name, price))
-        }
-        return products
+        val url = "https://catalog.onliner.by/sdapi/catalog.api/search/${category}?page=${page}&order=price:asc" //FIXME filter without price but remove price sorting
+        val json: JsonNode = getJsonResponse(url)
+        return mapProducts(json)
     }
 
     override fun getPriceStatistics(product: Product): PriceStatistics {
         val url = "https://catalog.api.onliner.by/products/${product.key}/prices-history?period=12m"
-        val httpEntity = restTemplate.getForEntity(url, String::class.java)
-        val json: JsonNode = jsonMapper.readTree(httpEntity.body)
-        val currency = Currency.valueOf(json["chart_data"]["currency"].asText())
+        val json = getJsonResponse(url)
+        val prices = mapPrices(json)
+        return PriceStatistics(product, prices)
+    }
+
+    private fun getJsonResponse(url: String): JsonNode {
+        val httpEntity: HttpEntity<String> = restTemplate.getForEntity(url, String::class.java)
+        return jsonMapper.readTree(httpEntity.body)
+    }
+
+    private fun mapProducts(json: JsonNode): ArrayList<Product> {
+        val products = arrayListOf<Product>()
+        json["products"].onEach {
+            products.add(mapProduct(it))
+        }
+        return products
+    }
+
+    private fun mapProduct(it: JsonNode): Product {
+        val id = it["id"].asLong()
+        val key = it["key"].asText()
+        val name = it["full_name"].asText()
+        val jsonPrice = it["prices"]["price_min"]
+        val price = Money(toDecimal(jsonPrice["amount"]), Currency.valueOf(jsonPrice["currency"].asText()))
+        return Product(id, key, name, price)
+    }
+
+    private fun mapPrices(json: JsonNode): ArrayList<PriceEntry> {
         val prices = arrayListOf<PriceEntry>()
+        val currency = Currency.valueOf(json["chart_data"]["currency"].asText())
         json["chart_data"]["items"].onEach loop@{
             if (it["price"].isNull) {
                 return@loop
             }
-            val price = Money(toDecimal(it["price"]), currency)
-            val parsed = formatter.parse(it["date"].asText())
-            val date = LocalDate.of(parsed.get(ChronoField.YEAR), parsed.get(ChronoField.MONTH_OF_YEAR), 1)
-            prices.add(PriceEntry(date, price))
+            prices.add(mapPriceEntry(it, currency))
         }
-        return PriceStatistics(product, prices)
+        return prices
+    }
+
+    private fun mapPriceEntry(it: JsonNode, currency: Currency): PriceEntry {
+        val price = Money(toDecimal(it["price"]), currency)
+        val parsed = formatter.parse(it["date"].asText())
+        val date = LocalDate.of(parsed.get(ChronoField.YEAR), parsed.get(ChronoField.MONTH_OF_YEAR), FIRST_DAY_OF_MONTH)
+        return PriceEntry(date, price)
     }
 
     private fun toDecimal(node: JsonNode) = BigDecimal.valueOf(node.asDouble())
