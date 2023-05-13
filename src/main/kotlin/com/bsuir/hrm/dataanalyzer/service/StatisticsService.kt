916003f94774b427d93712dd6cd91f362b361d22
herman2lv <herman2lv@gmail.com>
2023-05-13 18:17:39 +0300

    Add StatisticsService fun declarations

--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/service/StatisticsService.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/service/StatisticsService.kt
@@ -1,4 +1,13 @@
 package com.bsuir.hrm.dataanalyzer.service
 
+import com.bsuir.hrm.dataanalyzer.domain.Dataset
+import com.bsuir.hrm.dataanalyzer.domain.PriceStatistics
+import java.math.BigDecimal
+
 interface StatisticsService {
+
+    fun getInflationRateByMonths(prices: List<PriceStatistics>): Dataset
+
+    fun getInflationRateForYear(prices: List<PriceStatistics>): BigDecimal
+
 }
--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/service/impl/StatisticsServiceImpl.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/service/impl/StatisticsServiceImpl.kt
@@ -1,5 +1,17 @@
 package com.bsuir.hrm.dataanalyzer.service.impl
 
+import com.bsuir.hrm.dataanalyzer.domain.PriceStatistics
+import com.bsuir.hrm.dataanalyzer.domain.Dataset
 import com.bsuir.hrm.dataanalyzer.service.StatisticsService
+import java.math.BigDecimal
 
-class StatisticsServiceImpl : StatisticsService
+class StatisticsServiceImpl : StatisticsService {
+
+    override fun getInflationRateByMonths(prices: List<PriceStatistics>): Dataset {
+        TODO("Not yet implemented")
+    }
+
+    override fun getInflationRateForYear(prices: List<PriceStatistics>): BigDecimal {
+        TODO("Not yet implemented")
+    }
+}
