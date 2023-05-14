446e491429cf656a360e8dc73c5bb2510eff864a
herman2lv <herman2lv@gmail.com>
2023-05-14 17:52:53 +0300

    Expand ProductRepository

--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/data/ProductRepository.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/data/ProductRepository.kt
@@ -3,4 +3,8 @@ package com.bsuir.hrm.dataanalyzer.data
 import com.bsuir.hrm.dataanalyzer.domain.Product
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProductRepository: JpaRepository<Product, Long>
+interface ProductRepository : JpaRepository<Product, Long> {
+
+    fun findAllByCategoryIn(categories: List<String>): List<Product>
+
+}
