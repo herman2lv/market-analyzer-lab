6aa41c6337148ce960bff994aaab78d128fe5c53
herman2lv <herman2lv@gmail.com>
2023-05-14 17:52:22 +0300

    Update Dataset structure

--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/domain/Dataset.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/domain/Dataset.kt
@@ -1,9 +1,9 @@
 package com.bsuir.hrm.dataanalyzer.domain
 
 data class Dataset(
-    val labels: List<String>,
-    private val dataSeriesList: MutableList<DataSeries> = mutableListOf()
+    val labels: List<String>
 ) {
+    val dataSeriesList: MutableList<DataSeries> = mutableListOf()
 
     fun addDataSeries(vararg dataSeries: DataSeries) {
         this.dataSeriesList.addAll(dataSeries)
