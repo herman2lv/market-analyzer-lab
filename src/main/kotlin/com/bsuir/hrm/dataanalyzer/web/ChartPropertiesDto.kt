21ed163481d5cf0f2d236b1507e48f6ec54ee023
herman2lv <herman2lv@gmail.com>
2023-05-15 08:51:14 +0300

    ChartPropertiesDto: remove redundant annotation

--- src/main/kotlin/com/bsuir/hrm/dataanalyzer/web/ChartPropertiesDto.kt
+++ src/main/kotlin/com/bsuir/hrm/dataanalyzer/web/ChartPropertiesDto.kt
@@ -1,11 +1,9 @@
 package com.bsuir.hrm.dataanalyzer.web
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class ChartPropertiesDto(
     val categories: List<String>,
-    @JsonFormat(pattern = "yyyy-MM-dd")
     val start: LocalDate = LocalDate.now().minusYears(1),
     val end: LocalDate = LocalDate.now(),
 )
