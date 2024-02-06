c57dfa128b1592722ffd67c06cafbe837e9963e9
herman2lv <herman2lv@gmail.com>
2024-02-06 11:08:21 +0300

    service-discovery: add discovery service

---
+++ apps/service-discovery/build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.3"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+}
+
+group = "com.bsuir.hrm"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.0"
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		@Suppress("SuspiciousCollectionReassignment")
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "DataAnalyzer"
 
 include(":apps:legacy")
 include(":apps:data-extractor")
+include(":apps:service-discovery")
